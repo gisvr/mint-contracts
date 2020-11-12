494bac2d8dd6d1a365b38092e2de3e1faecb706c
liyu <txdev6@gmail.com>
2020-11-12 15:23:20 +0800

    add: config

--- networks.js
+++ networks.js
@@ -1,3 +1,5 @@
+const {projectId, adminKey} = require('../secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 module.exports = {
     networks: {
         development: {
@@ -8,5 +10,19 @@ module.exports = {
             gasPrice: 5e9,
             networkId: '*',
         },
+        rinkeby: {
+            provider: () => new HDWalletProvider(
+                adminKey, `https://rinkeby.infura.io/v3/${projectId}`
+            ),
+            networkId: 4,
+            gasPrice: 10e9
+        },
+        ropsten: {
+            provider: () => new HDWalletProvider(
+                adminKey, `https://ropsten.infura.io/v3/${projectId}`
+            ),
+            networkId: 3,
+            gasPrice: 10e9
+        }
     },
 };
